51b339c37c8fdc3e9e22377e14c9b500869e3062
Noah Stieler <noah.stieler@gmail.com>
2025-04-30 16:10:35 -0500

    Docs for run.bat

--- run.bat
+++ run.bat
@@ -1,5 +1,9 @@
-cd build
-cmake ../
-cmake --build .
-cd ..
+:: Generate project build system
+:: -S source directory, -B build directory
+cmake -S . -B build
+
+::Build the project
+cmake --build ./build
+
+::Run the executable
 "./build/src/Debug/Stoneware3D.exe"
